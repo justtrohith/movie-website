7a9462511d70b881eb8a75439fa86dcf6ccf813f
T Rohith Kumar <135516605+justtrohith@users.noreply.github.com>
2024-06-03 09:45:16 +0530

    css and js updates

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {useState} from 'react'
+import React from 'react'
+
+
+
+
+const App = () => {
+  const [counter, setCounter]= useState(0);
+
+  return (
+    <div className='App'>
+      <button onClick={()=>setCounter((prevCount)=>prevCount-1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={()=>setCounter((prevCount)=>prevCount+1)}>+</button>
+    </div>
+  )
+}
+
+export default App
